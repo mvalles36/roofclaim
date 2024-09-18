22b31f69691e4fca9e31125c9bf6c240abf0c479
Mark Valles <mvalles36@users.noreply.github.com>
2024-09-18 00:08:40 -0500

    Update index.js

--- src/integrations/supabase/index.js
+++ src/integrations/supabase/index.js
@@ -1,21 +1,21 @@
 import { supabase } from './supabase.js';
 import { SupabaseAuthProvider, useSupabaseAuth, SupabaseAuthUI } from './auth.jsx';
 import {
-  useCustomers,
-  useCustomer,
-  useAddCustomer,
-  useUpdateCustomer,
-  useDeleteCustomer
-} from './hooks/useCustomer.js';
+  useContacts,
+  useContacts,
+  useAddContacts,
+  useUpdateContacts,
+  useDeleteContacts
+} from './hooks/useContacts.js';
 
 export {
   supabase,
   SupabaseAuthProvider,
   useSupabaseAuth,
   SupabaseAuthUI,
-  useCustomers,
-  useCustomer,
-  useAddCustomer,
-  useUpdateCustomer,
-  useDeleteCustomer
+  useContacts,
+  useContacts,
+  useAddContacts,
+  useUpdateContacts,
+  useDeleteContacts
 };
