9468c74d4d875f06a9b966fefe1c5751db08eab1
Mark Valles <mvalles36@users.noreply.github.com>
2024-09-18 20:24:39 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,19 @@
-import { fileURLToPath, URL } from "url";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { resolve } from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'url';
+import { resolve } from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    host: "localhost",  // Changed from "::" to "localhost" for local development
-    port: 8080,          // Changed port to a number instead of a string
+    host: 'localhost',
+    port: 8080,
   },
   plugins: [react()],
   resolve: {
     alias: [
       {
-        find: "@",
-        replacement: fileURLToPath(new URL("./src", import.meta.url)),
-      },
-      {
-        find: "lib",
-        replacement: resolve(__dirname, "lib"),
+        find: '@',
+        replacement: fileURLToPath(new URL('./src', import.meta.url)),
       },
     ],
   },
