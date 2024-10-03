81402458428b5812098f6319ee68b5ee67d311d2
Mark Valles <mvalles36@users.noreply.github.com>
2024-10-03 14:13:11 -0500

    Update supabase.js

--- src/integrations/supabase/supabase.js
+++ src/integrations/supabase/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_KEY = 'SUPABASE_CLIENT_ANON_KEY'
+const SUPABASE_KEY = 'VITE_SUPABASE_CLIENT_ANON_KEY'
 const SUPABASE_URL = 'https://kblvkvmblwomrwlscqfv.supabase.co'
 const supabase = createClient(SUPABASE_URL, process.env.SUPABASE_KEY);
