c0b18876fb371a1aec9a293d6f0d4ddff5efc499
Mark Valles <mvalles36@users.noreply.github.com>
2024-10-03 21:39:26 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
+import { UserProvider } from "./context/UserContext"; // Import your UserProvider
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <UserProvider> {/* Wrap the app with UserProvider */}
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </UserProvider>
   </React.StrictMode>
 );
