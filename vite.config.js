0088882e75ee3d0a415f195a81201b32eaa26b4d
Mark Valles <mvalles36@users.noreply.github.com>
2024-10-12 20:57:16 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -12,5 +11,9 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: './index.html', // Ensure the entry point is specified
+    },
   },
-})
+  base: './', // This helps in resolving asset paths
+});
