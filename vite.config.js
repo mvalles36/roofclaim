608bc99c5acdf5c7a0e7e32f948ede3d0ec6d0d0
Mark Valles <mvalles36@users.noreply.github.com>
2024-10-14 13:31:02 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,25 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { fileURLToPath, URL } from "url";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: "::",
+    port: "8080",
+  },
   plugins: [react()],
   resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
+    alias: [
+      {
+        find: "@",
+        replacement: fileURLToPath(new URL("./src", import.meta.url)),
+      },
+      {
+        find: "lib",
+        replacement: resolve(__dirname, "lib"),
+      },
+    ],
   },
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: './index.html',  // Ensures Rollup knows where the entry point is
-      external: [],           // Add external dependencies if needed (empty for now)
-     },
-   },
-  base: './', // This helps in resolving asset paths
 });
